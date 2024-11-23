222cab073ebe8e5183854cbda5fac933eae24e64
wilfrenm <wilfrenm2003@gmail.com>
2024-11-23 18:18:30 +0530

    corrected it

--- InsideFolder/Inside/file1.php
+++ InsideFolder/Inside/file1.php
@@ -1,13 +1,16 @@
 <?php
 
-    $var="ffgbd";
+    $var="ffgb";
     $car="kodvmv";
     print_r($var);
+
     if(true){
         echo "ddf";
         function pub(){
             echo "hello";
         }
     }
+
+    echo "dsmvkpodmvpodsvbf";
 
 ?>
